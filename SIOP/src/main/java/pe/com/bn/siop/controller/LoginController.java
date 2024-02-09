5b37883e916d8e0616fb9e36bfea95a54de9765c
Percy Anchante <percyandersson12@gmail.com>
2024-02-09 06:29:47 -0500

    Cambio con el ear y vista

---
+++ SIOP/src/main/java/pe/com/bn/siop/controller/LoginController.java
@@ -0,0 +1,37 @@
+package pe.com.bn.siop.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class LoginController {
+
+	
+
+
+    @RequestMapping(value="/a.htm")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+
+        return new ModelAndView("index");
+    }
+    
+    
+
+    
+    
+    
+    
+}
---
+++ SIOP/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>	
+<h3>Numero</h3>
+
+
+<form>
+	
+	<h2>Form</h2>
+	
+	
+	<label>Nombre: </label>
+	
+	
+	<input type="text" value="nombre" />
+	
+	
+	<label>Apellido: </label>
+	
+	<input type="text" value="apellido" />
+	
+	<button  type="submit" >
+		Subir
+	</button>
+	
+	
+
+</form>
+
+</body>
+</html>
